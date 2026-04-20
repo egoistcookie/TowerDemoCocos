0bce596fd6ed686096d395b21002e08be9ac17b4
egoistcookie <335985579@qq.com>
2026-04-21 00:24:07 +0800

    feat：更新版本信息；

--- database/cocos-1.sql
+++ database/cocos-1.sql
@@ -25,10 +25,10 @@ select DATE(created_at),count(*) from game_sessions
 --选卡明细表
 select * from card_selection_events  order by created_at desc ;
 --统计每天有多少次抽卡
-select * from card_selection_events where DATE(created_at) = CURDATE()
+select session_id,player_id,level,selection_mode,cards_json,created_at  from card_selection_events where DATE(created_at) = CURDATE()
  and player_id not in ('player_1772462826043_800','player_1772466497770_5671','player_1772530937065_3381','player_1775652153130_8335',
 'player_1772722064044_978','player_1772465771074_4106') order by created_at desc ;
-select * from card_selection_events where DATE(created_at) = CURDATE()
+select session_id,player_id,level,selection_mode,cards_json,created_at from card_selection_events where DATE(created_at) = CURDATE()
  and player_id  in ('player_1772462826043_800','player_1772466497770_5671','player_1772530937065_3381','player_1775652153130_8335',
 'player_1772722064044_978','player_1772465771074_4106') order by created_at desc ;
 --统计每天有多少广告抽卡(reroll和get_all只有看完了视频才会触发)
